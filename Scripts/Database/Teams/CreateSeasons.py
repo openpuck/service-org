6eb3c36943b27eaa18660dea95ba5aad82e986d6
Grant Cohoe <grant@grantcohoe.com>
2016-09-11 17:59:26 -0400

    some DB shenanigans

--- Scripts/Database/Teams/CreateSeasons.py
+++ Scripts/Database/Teams/CreateSeasons.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 
 import sys
+import os
 from boto.dynamodb2.fields import HashKey, RangeKey, GlobalAllIndex
 from boto.dynamodb2.table import Table
 from boto.exception import JSONResponseError
 from boto.dynamodb2.types import NUMBER
 
-sys.path.insert(0, '../')
+here = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(here, "../"))
 from Connection import remoteconn as conn
+
 
 table_name = "seasons"
 
--- Scripts/Database/Teams/CreateTeams.py
+++ Scripts/Database/Teams/CreateTeams.py
@@ -1,19 +1,32 @@
 #!/usr/bin/env python
 
 import sys
+import os
 from boto.dynamodb2.fields import HashKey, RangeKey, GlobalAllIndex
 from boto.dynamodb2.table import Table
 from boto.exception import JSONResponseError
 from boto.dynamodb2.types import NUMBER
+import time
 
-sys.path.insert(0, '../')
+here = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(here, "../"))
 from Connection import remoteconn as conn
 
 table_name = "teams"
 
 try:
     teams_table = Table(table_name, connection=conn)
+    if teams_table.describe()['Table']['TableStatus'] == "CREATING":
+        while teams_table.describe()['Table']['TableStatus'] == "CREATING":
+            print "Waiting for CREATING to be done..."
+            time.sleep(1)
+        print "Table is done."
     teams_table.delete()
+    while teams_table.describe()['Table']['TableStatus'] == "DELETING":
+        print "Waiting for DELETING to be done..."
+        time.sleep(1)
+    print "Table is done."
+
 except JSONResponseError:
     print "Table '%s' does not exist." % table_name
 
