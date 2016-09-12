0f7dd88e7f9fdb567cea39ef155bab9efd41646c
Grant Cohoe <grant@grantcohoe.com>
2016-09-11 23:31:59 -0400

    read functions

---
+++ league/functions/read/handler.py
@@ -0,0 +1,41 @@
+from __future__ import print_function
+
+import json
+import logging
+
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
+# this adds the component-level `lib` directory to the Python import path
+import sys, os
+# get this file's directory independent of where it's run from
+here = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(here, "../"))
+sys.path.append(os.path.join(here, "../vendored"))
+
+# import the shared library, now anything in component/lib/__init__.py can be
+# referenced as `lib.something`
+import lib
+
+
+def handler(event, context):
+    log.debug("Received event {}".format(json.dumps(event)))
+
+    # Test for required attributes
+    if 'pathId' not in event:
+        raise lib.exceptions.BadRequestException("Key 'id' is missing.")
+    if event['pathId'] == "":
+        raise lib.exceptions.BadRequestException("Key 'id' is missing.")
+
+    # Get response
+    try:
+        response = lib.LeaguesTable.get_item(Key={'id': event['pathId']})
+    except lib.exceptions.ClientError as ce:
+        raise lib.exceptions.InternalServerException(ce.message)
+
+    if 'Item' not in response:
+        raise lib.exceptions.NotFoundException(
+            "Object '%s' not found." % event['pathId'])
+
+    # Return
+    return lib.get_json(response['Item'])
