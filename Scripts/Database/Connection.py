ef63d868505e26db1419220e528c81600d911283
Grant Cohoe <grant@grantcohoe.com>
2016-09-11 20:55:18 -0400

    shuffled some functions around and added createleagues

--- Scripts/Database/Connection.py
+++ Scripts/Database/Connection.py
@@ -1,5 +1,7 @@
 from boto.dynamodb2.table import Table
 from boto.dynamodb2.layer1 import DynamoDBConnection
+import time
+from boto.exception import JSONResponseError
 
 localconn = DynamoDBConnection(host='localhost', port=8000, aws_access_key_id='dummy', aws_secret_access_key='dummy', is_secure=False)
 remoteconn = DynamoDBConnection()
@@ -11,3 +13,21 @@ Seasons = Table('seasons', connection=localconn)
 SeasonPhases = Table('season_phases', connection=localconn)
 Locations = Table('locations', connection=localconn)
 LocationAltnames = Table('location_altnames', connection=localconn)
+
+
+def clear_table(conn, table_name):
+    try:
+        teams_table = Table(table_name, connection=conn)
+        if teams_table.describe()['Table']['TableStatus'] == "CREATING":
+            while teams_table.describe()['Table']['TableStatus'] == "CREATING":
+                print "Waiting for CREATING to be done..."
+                time.sleep(1)
+            print "Table is done."
+        teams_table.delete()
+        while teams_table.describe()['Table']['TableStatus'] == "DELETING":
+            print "Waiting for DELETING to be done..."
+            time.sleep(1)
+        print "Table is done."
+
+    except JSONResponseError:
+        print "Table '%s' does not exist." % table_name
---
+++ Scripts/Database/Leagues/CreateLeagues.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import sys
+import os
+from boto.dynamodb2.fields import HashKey, RangeKey, GlobalAllIndex
+from boto.dynamodb2.table import Table
+
+here = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(here, "../"))
+from Connection import remoteconn as conn
+from Connection import clear_table
+
+table_name = "leagues"
+clear_table(conn, table_name)
+
+AbbrIndex = GlobalAllIndex("AbbrIndex",
+                           parts=[
+                               HashKey("abbr")
+                           ],
+                           throughput={
+                               'read': 1,
+                               'write': 1,
+                           })
+
+new_table = Table.create(table_name,
+                         schema=[
+                             HashKey("id")
+                         ],
+                         throughput={
+                             'read': 1,
+                             'write': 1
+                         },
+                         global_indexes=[
+                             AbbrIndex,
+                         ],
+                         connection=conn)
+
+print "Created '%s' table" % table_name
