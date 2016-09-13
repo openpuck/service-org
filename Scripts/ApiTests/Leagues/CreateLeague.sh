875ad01d909a17261a6cef921f99971ce28f1833
Grant Cohoe <grant@grantcohoe.com>
2016-09-12 21:36:09 -0400

    Added league create

--- Scripts/ApiTests/Leagues/CreateLeague.sh
+++ Scripts/ApiTests/Leagues/CreateLeague.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-DEBUG=0
-if [[ ${DEBUG} == 1 ]]; then
-    DEBUG_OPTS="-v"
-else
-    DEBUG_OPTS=""
-fi
 # Common Stuff
-CURL="curl -s ${DEBUG_OPTS} -H 'Content-Type: application/json'"
+CURL="curl -v -H 'Content-Type: application/json'"
 
 # Method-specific configuration
 URL="https://mkvusbh1l7.execute-api.us-east-1.amazonaws.com/dev/league"
-METHOD="GET"
-PAYLOAD=''
+METHOD="POST"
+PAYLOAD='{"abbr": "FOOBAR", "name": "foobar", "website": "lolz"}'
 
 # Execute
 eval ${CURL} -X ${METHOD} ${URL} -d \'${PAYLOAD}\' | json_pp
