48524ed43db3283b6601aa7f790e37eb28c8cc28
Grant Cohoe <grant@grantcohoe.com>
2016-09-12 22:36:09 -0400

    League Delete

---
+++ Scripts/ApiTests/Leagues/DeleteLeague.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Load in our common stuff.
+source ../Common.sh
+
+# Test-specific vars
+METHOD="DELETE"
+PAYLOAD=''
+ENDPOINT="/883c9dbf-5aae-400e-8110-84b68cee6838"
+
+# Execute
+#echo "${URL}/${ENDPOINT}"
+eval ${CURL} -X ${METHOD} ${URL}${ENDPOINT} -d \'${PAYLOAD}\' | json_pp
