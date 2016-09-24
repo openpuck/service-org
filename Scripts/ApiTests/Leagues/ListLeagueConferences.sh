d6d0f0145b6f33d7acad6ee9fa859b198894ba2f
Grant Cohoe <grant@grantcohoe.com>
2016-09-24 17:30:03 -0400

    added league conference lookups

---
+++ Scripts/ApiTests/Leagues/ListLeagueConferences.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Load in our common stuff.
+source ../Common.sh
+
+# Test-specific vars
+METHOD="GET"
+PAYLOAD=''
+ENDPOINT="/league/ac99003b-845d-4cec-9c02-4dfe1acc1839/conferences"
+
+# Execute
+#echo "${URL}/${ENDPOINT}"
+eval ${CURL} -X ${METHOD} ${URL}${ENDPOINT} -d \'${PAYLOAD}\' | json_pp
---
+++ league/functions/conferences/conflist/handler.py
@@ -0,0 +1,38 @@
+from __future__ import print_function
+
+import json
+import logging
+
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
+# this adds the component-level `lib` directory to the Python import path
+import sys, os
+# get this file's directory independent of where it's run from
+here = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(here, "../../"))
+sys.path.append(os.path.join(here, "../../vendored"))
+
+# import the shared library, now anything in component/lib/__init__.py can be
+# referenced as `lib.something`
+import lib
+from boto3.dynamodb.conditions import Key
+
+def handler(event, context):
+    log.debug("Received event {}".format(json.dumps(event)))
+
+    # Test for required attributes
+    if 'pathId' not in event:
+        raise lib.exceptions.BadRequestException("Key 'id' is missing.")
+    if event['pathId'] == "":
+        raise lib.exceptions.BadRequestException("Key 'id' is missing.")
+
+
+    try:
+        result = lib.ConferencesTable.query(
+            IndexName='ConfByLeagueGender',
+            KeyConditionExpression=Key('league').eq(event['pathId'])
+        )
+        return lib.get_json(result['Items'])
+    except lib.exceptions.ClientError as ce:
+        raise lib.exceptions.InternalServerException(ce.message)
