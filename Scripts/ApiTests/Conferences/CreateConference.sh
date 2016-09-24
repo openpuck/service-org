6108566ec10be121f2f9f52c222ef74ea895eda3
Grant Cohoe <grant@grantcohoe.com>
2016-09-24 16:29:34 -0400

    removed todo

--- Scripts/ApiTests/Conferences/CreateConference.sh
+++ Scripts/ApiTests/Conferences/CreateConference.sh
@@ -5,7 +5,6 @@ source ../Common.sh
 
 # Test-specific vars
 
-# @TODO: Test for is_women being not "yes" or "no", league should exist...
 METHOD="POST"
 PAYLOAD='{"abbr": "FOOBAR", "cn": "foobar", "website": "lolz", "is_women": "yes", "league": "foobar"}'
 ENDPOINT="/conference"
