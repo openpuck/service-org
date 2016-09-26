f238457cd0b8ad0275e6e266d3821e9cd3ba9f06
Grant Cohoe <grant@grantcohoe.com>
2016-09-25 23:09:12 -0400

    Added team delete

---
+++ Scripts/ApiTests/Teams/DeleteTeam.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Load in our common stuff.
+source ../Common.sh
+
+# Test-specific vars
+METHOD="DELETE"
+PAYLOAD=''
+ENDPOINT="/team/39162250-c85b-438d-a6ac-79e110f11c22"
+
+# Execute
+#echo "${URL}/${ENDPOINT}"
+eval ${CURL} -X ${METHOD} ${URL}${ENDPOINT} -d \'${PAYLOAD}\' | json_pp
---
+++ team/functions/delete/handler.py
@@ -0,0 +1,36 @@
+from __future__ import print_function
+
+import json
+import logging
+
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
+# this adds the component-level `lib` directory to the Python import path
+import sys, os
+# get this file's directory independent of where it's run from
+here = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(here, "../../"))
+sys.path.append(os.path.join(here, "../../vendored"))
+
+# import the shared library, now anything in component/lib/__init__.py can be
+# referenced as `lib.something`
+import lib
+from boto3.dynamodb.conditions import Attr
+
+
+def handler(event, context):
+    log.debug("Received event {}".format(json.dumps(event)))
+
+    # Return
+    try:
+        lib.TeamsTable.delete_item(
+                               Key={'id': event['pathId']},
+                               ConditionExpression=Attr('id').eq(event['pathId'])
+                           )
+        return {}
+    except lib.exceptions.ClientError as ce:
+        if "ConditionalCheckFailedException" in ce.message:
+            raise lib.exceptions.NotFoundException("Object '%s' not found." % event['pathId'])
+
+        raise lib.exceptions.InternalServerException(ce.message)
